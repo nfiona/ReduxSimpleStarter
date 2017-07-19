9f901ef123ec8622ddd28015fbcae0954c267689
fiona <h.natapra@gmail.com>
2017-07-18 20:23:47 -0400

    created SearchBar component, linked to index.js

---
+++ src/components/search_bar.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+// define searcbar component
+const SearchBar = () => {
+  return <input />;
+};
+
+// make component available globally.
+export default SearchBar;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+// define full path to access a file
+import SearchBar from './components/search_bar';
+
 const API_KEY = 'AIzaSyD8hkVlfY3ZivaU1TT8h0hJPCxsl_CAXKI';
 
 // Create a new component that should produce HTML. The below is a class, written in ES6.
+// Render imported file.
 const App = () => {
-  return <div>Hi!</div>;
+  return (
+    <div>
+      <SearchBar />
+    </div>
+  );
 }
 
 // Render the component into the DOM. Instatiate the component name, and
