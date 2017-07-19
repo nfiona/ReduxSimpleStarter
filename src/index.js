88b43e230462fea275b28b6edbd808d6fe9a741e
fiona <h.natapra@gmail.com>
2017-07-18 20:10:32 -0400

    created components files, installed youtube api

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-// Create a new component that should produce HTML. The below is a class.
-const App = function() {
+const API_KEY = 'AIzaSyD8hkVlfY3ZivaU1TT8h0hJPCxsl_CAXKI';
+
+// Create a new component that should produce HTML. The below is a class, written in ES6.
+const App = () => {
   return <div>Hi!</div>;
 }
 
-// Render the component into the DOM. Instatiate the component name, and define a target container as a second argument (see index.html)
+// Render the component into the DOM. Instatiate the component name, and
+// define a target container as a second argument (see index.html)
 ReactDOM.render(<App />, document.querySelector('.container'));
