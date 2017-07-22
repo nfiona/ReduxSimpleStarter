faee7a2243559be075f89a6b534bce6589748ace
fiona <h.natapra@gmail.com>
2017-07-22 11:51:04 -0400

    Activated youtube-search-api, refactored App to class-based component, initialized a state

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 
-// create component using class and give access to all functionality that React.Component has.
+// create component using class (Class-Based Component) and give access to all functionality that React.Component has.
 // Class-based component must have a render method. Render functions must return a JSX.
 class SearchBar extends Component {
   // initialize state inside a constructor method.
@@ -13,21 +13,19 @@ class SearchBar extends Component {
 
   render() {
     // set new State. Input's value equals to current state.
+    // Setting our Input element as a Controlled Component by setting its value as state.
     // When input value changes, update the state (OnChange Event Handler). Then, when the
     // component re-renders, update the input's value to the new (manipulated) state.
     return (
-      <div>
-        <input
-            value={this.state.term}
-            onChange={ event => this.setState({ term: event.target.value}) }
-          />
-        Current Input's Value = {this.state.term}
-      </div>
-    );
+        <div>
+          <input
+              value={this.state.term}
+              onChange={ event => this.setState({ term: event.target.value}) }
+            />
+            Current Input's Value = {this.state.term}
+        </div>
+      );
     }
-
-
-
 
 
 
--- src/index.js
+++ src/index.js
@@ -1,20 +1,35 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
-
+import YTSearch from 'youtube-api-search';
 // define full path to access a file
 import SearchBar from './components/search_bar';
-
 const API_KEY = 'AIzaSyD8hkVlfY3ZivaU1TT8h0hJPCxsl_CAXKI';
 
-// Create a new component (functional component)that should produce HTML. The below is a class, written in ES6.
-// Render imported file.
-const App = () => {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  );
+// Refactor Functional Component to Class-Based. Initialize a state.
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { videos: [ ] };
+
+    // youtube-api-search
+    YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+      this.setState({ videos });
+      // equals to this.setState( { videos: videos });
+    });
+
+  }
+  
+  render() {
+    return (
+      <div>
+        <SearchBar />
+      </div>
+    );
+  }
 }
+
+
 
 // Render the component into the DOM. Instatiate the component name, and
 // define a target container as a second argument (see index.html)
