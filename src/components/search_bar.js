719b25e480e7064636424a9aa66103a206bace15
fiona <h.natapra@gmail.com>
2017-07-22 10:44:29 -0400

    initialized & manipulate State

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,17 +1,37 @@
 import React, { Component } from 'react';
 
-// define searchbar component (the below is functional component).
-    // const SearchBar = () => {
-    //   return <input />;
-    // };
-
 // create component using class and give access to all functionality that React.Component has.
 // Class-based component must have a render method. Render functions must return a JSX.
 class SearchBar extends Component {
+  // initialize state inside a constructor method.
+  constructor (props) {
+    super(props);
+
+    this.state = { term: ''};
+  }
+
+
   render() {
-    // create an input element, and pass an Event Handler as 'prop={value}'. See more props on React Docs.
-    return <input onChange={event => console.log(event.target.value)} />;
+    // set new State. Input's value equals to current state.
+    // When input value changes, update the state (OnChange Event Handler). Then, when the
+    // component re-renders, update the input's value to the new (manipulated) state.
+    return (
+      <div>
+        <input
+            value={this.state.term}
+            onChange={ event => this.setState({ term: event.target.value}) }
+          />
+        Current Input's Value = {this.state.term}
+      </div>
+    );
     }
+
+
+
+
+
+
+
   }
 
 // export/ make component available globally.
